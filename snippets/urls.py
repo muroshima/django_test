d1e1cee205d193076096f8e13538576b3805315c
muroshima <103478594+muroshima@users.noreply.github.com>
2022-09-29 13:13:30 +0900

    first commit

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
 from snippets import views
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 urlpatterns = [
     path("snippets/", views.snippet_list),
     path("snippets/<int:pk>/", views.snippet_detail),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # Redocの設定
+    path(
+        "redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
